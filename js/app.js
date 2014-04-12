a9cbceb805ef15ae7d70d773bd1db2ddef6c77b6
sandro <hello@alessndro.co.uk>
2014-04-12 20:56:46 +0100

    Basic UI for altering background render options

--- js/app.js
+++ js/app.js
@@ -1,5 +1,44 @@
-var t = new Trianglify();
-var width = parseInt($("#bg-width").val());
-var height = parseInt($("#bg-height").val());
-var pattern = t.generate(width, height);
-document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+  $('#noise-slider').slider({ 
+    max: 1,
+    min: 0,
+    value: 0.5,
+    step: 0.01,
+    slide: function(e,ui) {
+      $('#noise-value').html(ui.value);
+      generateBackground();
+    }
+  });
+
+  $('#cell-slider').slider({ 
+    max: 1000,
+    min: 0,
+    value: 150,
+    step: 10,
+    slide: function(e,ui) {
+      $('#cell-value').html(ui.value);
+      generateBackground();
+    }
+  });
+
+$("#generate").on('click', function() {
+  generateBackground();
+});
+
+function generateBackground() {
+  var noise = $('#noise-slider').slider("value");
+  var cellSize = $('#cell-slider').slider("value");
+
+  var t = new Trianglify({"noiseIntensity": noise,
+                          "cellsize": cellSize});
+
+  var width = parseInt($("#bg-width").val());
+  var height = parseInt($("#bg-height").val());
+  var pattern = t.generate(width, height);
+
+  $("#the-image").css({"background-image": pattern.dataUrl,
+                      "width": width,
+                      "height": height
+                      });
+
+  //$("#the-image").replaceWith(pattern.svgString);
+}

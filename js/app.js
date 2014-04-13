ba1bda33d539c4742ec1adb3ca8e64ab013dd9e3
sandro <hello@alessndro.co.uk>
2014-04-13 13:16:11 +0100

    Major UI changes

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ $('#noise-slider').slider({
 });
 
 $('#cell-slider').slider({ 
-  max: 1000,
-  min: 0,
+  max: 500,
+  min: 10,
   value: 150,
   step: 10,
   slide: function(e,ui) {
@@ -26,6 +26,11 @@ $('#cell-slider').slider({
   }
 });
 
+var currentTrianglifier;
+
+// Generate a pattern for when the page loads
+generateBackground();
+
 $("#generate").on('click', function() {
   generateBackground();
 });
@@ -35,25 +40,24 @@ function generateBackground() {
   var cellSize = $('#cell-slider').slider("value");
 
   var t = new Trianglify({"noiseIntensity": noise,
-                          "cellsize": cellSize});
+                          "cellsize": cellSize,
+                          "x_gradient": ['#00A0B0','#6A4A3C','#CC333F','#EB6841','#EDC951']
+                        });
 
   var width = parseInt($("#bg-width").val());
   var height = parseInt($("#bg-height").val());
   var pattern = t.generate(width, height);
 
-  $("#the-image").css({"background-image": pattern.dataUrl,
-                      "width": width,
-                      "height": height
-                      });
+  $("#backgrounds").css({"background-image": pattern.dataUrl});
+  currentTrianglifier = t;
 
   //$("#the-image").replaceWith(pattern.svgString);
 }
 
 $("#convert").on('click', function() {
-  var noise = $('#noise-slider').slider("value");
   var cellSize = $('#cell-slider').slider("value");
 
-  var t = new Trianglify({"noiseIntensity": noise,
+  var t = new Trianglify({"noiseIntensity": 0,
                           "cellsize": cellSize});
 
   var width = parseInt($("#bg-width").val());
@@ -62,5 +66,23 @@ $("#convert").on('click', function() {
   canvg('canvas', pattern.svgString);
   var canvas = document.getElementById("canvas");
   var img = canvas.toDataURL("image/png");
-  $(".main").append($('<img></img>').prop('src', img));
+  $("#downloadable-image").replaceWith($("<img id='downloadable-image'></img>").prop('src', img));
+  window.open(img);
 });
+
+// $.ajax({ 
+//     type: "GET",
+//     url: "http://www.colourlovers.com/api/palettes/top?jsonCallback=?", 
+//     data: { numResults: 10 },
+//     dataType: 'json',
+//     success: function(data){        
+//         $(data).each(function() {
+//           $("#output").append("<p>" + this["title"] + "</p>");
+//             $("#output").append("[");
+//             $(this["colors"]).each(function(){
+//                 $("#output").append("'#" + this + "',");
+//             });
+//               $("#output").append("]");
+//         });
+//     }
+// });

77a3d912a6dc5a0d4e519801b5db30154acd03be
sandro <hello@alessndro.co.uk>
2014-04-14 21:17:58 +0100

    Big refactoring, mainly for selecting colours

--- js/app.js
+++ js/app.js
@@ -17,8 +17,11 @@ var triOptions = {
   cellSize: function(){
     return $('#cell-slider').slider("value");
   },
-  noise: function(){
-    return $('#noise-slider').slider("value");
+  cellpadding: function(){
+    return $('#cellpadding-slider').slider("value");
+  },
+  bleed: function(){
+    return $('#bleed-slider').slider("value");
   },
   xColors: function(){
     return currentXPalette.colors;
@@ -32,20 +35,22 @@ initiateSliders();
 getColourSchemes(NO_OF_PALETTES_TO_RETRIEVE);
 
 function setNewTrianglifier() {
-  currentTrianglifier = new Trianglify({"noiseIntensity": triOptions.noise(),
+  currentTrianglifier = new Trianglify({"bleed": triOptions.bleed(),
                           "cellsize": triOptions.cellSize(),
                           "x_gradient": triOptions.xColors(),
-                          "y_gradient": triOptions.yColors()
+                          "y_gradient": triOptions.yColors(),
+                          "cellpadding": triOptions.cellpadding(),
+                          "noiseIntensity": 0
                         });
 }
 
-$("#generate").on('click', function() {
+function updateScreen() {
   setNewTrianglifier();
   generateBackground();
-});
+}
 
-$("#download").on('click', function() {
-  convertSVGtoPNG();
+$("#generate").on('click', function() {
+  updateScreen();
 });
 
 function generateBackground() {
@@ -92,56 +97,24 @@ function getColourSchemes(limit) {
 
           palettes.push(new Palette(palette_name, colors));
         });
-        addColourSchemeControls();
+        addColourList();
         currentXPalette = palettes[0];
         currentYPalette = palettes[0];
-        setNewTrianglifier();
-        generateBackground();
+        updateScreen();
         $("#loading").fadeOut(500);
       }
   });
 }
 
-function addColourSchemeControls() {
-  var select = $("<select id='colour-scheme-select-x' name='colour-scheme-x'>");
-
-  $(palettes).each(function(index){
-    $(select).append($("<option></option>").attr("value", index).text(this.name));
-  });
-
-  $("#generate").before(select);
-
-  $("#colour-scheme-select-x").on("change", function(){
-    currentXPalette = palettes[$(this).val()];
-    setNewTrianglifier();
-    generateBackground();
-  });
-
-  select = $("<select id='colour-scheme-select-y' name='colour-scheme'>");
-
-  $(palettes).each(function(index){
-    $(select).append($("<option></option>").attr("value", index).text(this.name));
-  });
-
-  $("#generate").before(select);
-
-  $("#colour-scheme-select-y").on("change", function(){
-    currentYPalette = palettes[$(this).val()];
-    setNewTrianglifier();
-    generateBackground();
-  });
-}
-
 function initiateSliders() {
-  $('#noise-slider').slider({
-    max: 1,
-    min: 0,
-    value: 0,
-    step: 0.1,
-    slide: function(e,ui) {
-      $('#noise-value').html(ui.value);
-      setNewTrianglifier();
-      generateBackground();
+  $('#bleed-slider').slider({
+    max: 500,
+    min: 10,
+    value: 150,
+    step: 5,
+    slide: function(e, slider) {
+      $('#bleed-value').html(slider.value);
+      updateScreen();
     }
   });
 
@@ -149,22 +122,62 @@ function initiateSliders() {
     max: 500,
     min: 10,
     value: 150,
-    step: 10,
-    slide: function(e,ui) {
-      $('#cell-value').html(ui.value);
-      setNewTrianglifier();
-      generateBackground();
+    step: 5,
+    slide: function(e, slider) {
+      $('#cell-value').html(slider.value);
+      updateScreen();
+    }
+  });
+
+  $('#cellpadding-slider').slider({
+    max: 500,
+    min: 10,
+    value: 15,
+    step: 5,
+    slide: function(e, slider) {
+      $('#cellpadding-value').html(slider.value);
+      updateScreen();
     }
   });
 }
 
 $(".screen-size-input").on("change", function(){
-  setNewTrianglifier();
-  generateBackground();
+  updateScreen();
 });
 
- $("#randomise-colours").on("click", function(){
-    // Need to call change() to trigger the event handler so the background is updated
-    $("#colour-scheme-select-x").val(Math.floor(Math.random() * NO_OF_PALETTES_TO_RETRIEVE)).change();
-    $("#colour-scheme-select-y").val(Math.floor(Math.random() * NO_OF_PALETTES_TO_RETRIEVE)).change();
+$("#randomise-colours").on("click", function(){
+  currentXPalette = palettes[(Math.floor(Math.random() * NO_OF_PALETTES_TO_RETRIEVE))];
+  currentYPalette = palettes[(Math.floor(Math.random() * NO_OF_PALETTES_TO_RETRIEVE))];
+  updateScreen();
+});
+
+$("#match-colours").on("click", function(){
+  currentYPalette = currentXPalette;
+  updateScreen();
+});
+
+function addColourList() {
+  $(palettes).each(function(index){
+    var cont = $("<div class='palette-cont clearfix' data-palette-index='" + index + "'></div>");
+    var palette = this;
+    var noOfColors = palette.colors.length;
+
+    $(palette.colors).each(function(){
+      var paletteColor = $("<span class='palette-colour'></span>");
+      $(paletteColor).css({"width": (100 / noOfColors) + "%",
+                            "background-color": this});
+      $(cont).append(paletteColor);
+    });
+    $("#colour-list").append(cont);
   });
+
+  $("#colour-list").on("click", ".palette-cont", function() {
+    var paletteIndex = $(this).data("palette-index");
+    if ($("[type='radio']:checked").val() === "x") {
+      currentXPalette = palettes[paletteIndex];
+    } else {
+      currentYPalette = palettes[paletteIndex];
+    }
+    updateScreen();
+  });
+}
